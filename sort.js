4ae80c5d698e076e28b892ae5fab21bd1c06e1ba
likaihua <kaihua.li@fengjr.com>
2018-03-10 16:16:38 +0800

    add selectionSort function

--- sort.js
+++ sort.js
@@ -4,19 +4,36 @@
  * @returns {*}
  */
 function bubbleSort(arr){
-  var i = 0,
-    j = 0;
-  for(i=1; i<arr.length; i++){
-    for(j=0; j<=arr.length-i; j++){
-      var temp = 0;
-      // ">" 从小到大排序
-      // "<" 从大到小排序
-      if(arr[j] > arr[j+1]){
-        temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j+1]) {        //相邻元素两两对比
+        var temp = arr[j+1];        //元素交换
+        arr[j+1] = arr[j];
+        arr[j] = temp;
       }
     }
+  }
+  return arr;
+}
+
+/***
+ * 选择排序
+ * @param arr
+ */
+function selectionSort(arr) {
+  var len = arr.length;
+  var minIndex = 0;
+  for(var i = 0;i<len-1;i++){
+    minIndex = i;
+    for(var j = i+1;j<len;j++ ){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
   }
   return arr;
 }
